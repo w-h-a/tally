3aa41829cc8b97d35951dde6983341263b4b8ab3
Wes Anderson <wesley.herb.anderson@gmail.com>
2026-04-21 06:46:30 -0700

    feat: add consensus port interface and options

---
+++ internal/client/consensus/consensus.go
@@ -0,0 +1,18 @@
+package consensus
+
+import (
+	"context"
+
+	api "github.com/w-h-a/tally/proto/log/v1"
+)
+
+type Consensus interface {
+	Apply(ctx context.Context, data []byte) (uint64, error)
+	AddVoter(ctx context.Context, id string, addr string) error
+	RemoveServer(ctx context.Context, id string) error
+	Leader(ctx context.Context) string
+	GetServers(ctx context.Context) ([]*api.Server, error)
+	State(ctx context.Context) string
+	WaitForLeader(ctx context.Context) error
+	Close(ctx context.Context) error
+}
